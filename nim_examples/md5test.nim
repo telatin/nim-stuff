f4d798c0f389fa67f6f9a9848110128a261a24b8
Andrea Telatin <andrea@telatin.com>
2020-08-04 13:39:29 +0100

    random examples

---
+++ nim_examples/md5test.nim
@@ -0,0 +1,8 @@
+import md5
+
+let
+  list = ["andrea telatin", "1", "3", "asdo aiusdoauhefouhaei fuh ", "asuhdahefgohsrpigjapgjpsdjrgphsdrufndlskjnkaljsdhfiahsrugihspdiugjpsdtjhsdfjthishdgohasuyrfgauyisegdfitafsgefugaosrhgpisudthjpisdtbòksdmfòkvjandsfng", "asdhoagdfuagshrguha uh aisrh apiuhrg ipuahrg asihga isuhf svzkjxd vlzbfjggzsof gaysu hpaiuhr gpiahr goysh ghprigu hpiughsp iur gpsig "]
+
+for i in list:
+  echo "> ", i
+  echo toMD5(i)
